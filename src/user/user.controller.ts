79332dd053950a313553f04e795232f464db7f62
Gilmar Oliveira <giilmaroliveira@outlook.com>
2021-11-03 18:36:08 -0300

    Add method for search user by username

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { User } from './user.entity';
 import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @Get(':username')
+  public getByUsername(@Param('username') username: string) {
+    const user = this.userService.getByUsername(username);
+    return user;
+  }
+
   @Post()
-  public post(@Body() user) {
+  public post(@Body() user: User) {
     const userCreated = this.userService.post(user);
     return userCreated;
   }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
 
 @Injectable()
 export class UserService {
-  private users = [];
+  private users: Array<User> = [
+    {
+      id: 1,
+      username: 'gilmar',
+      email: 'gilmar@mail.com',
+      password: '123456',
+      fullName: 'Gilmar Oliveira',
+      registerDate: new Date(),
+    },
+  ];
 
-  public post(user) {
+  public post(user): User {
     this.users.push(user);
     return user;
+  }
+
+  public getByUsername(username: string): User {
+    return this.users.find((user) => user.username === username);
   }
 }
