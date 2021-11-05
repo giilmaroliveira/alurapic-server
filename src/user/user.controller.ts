b01f4744ee896ac366973023f2f902d2f0a8bfda
Gilmar Oliveira <giilmaroliveira@outlook.com>
2021-11-05 15:30:59 -0300

    Adding validation when user does not exist

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Param, HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  HttpStatus,
+  NotFoundException,
+} from '@nestjs/common';
 import { NestResponse } from '../core/http/nest-response';
 import { NestResponseBuilder } from '../core/http/nest-response-builder';
 import { User } from './user.entity';
@@ -11,6 +19,12 @@ export class UserController {
   @Get(':username')
   public getByUsername(@Param('username') username: string) {
     const user = this.userService.getByUsername(username);
+    if (!user) {
+      throw new NotFoundException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'User not found',
+      });
+    }
     return user;
   }
 
